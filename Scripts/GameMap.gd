7ae9af5cb48c4e275a25f4b0db68d41c971f0f7a
Kevin Xue <ivexnuke@gmail.com>
2019-12-11 23:40:13 -0400

    Moved some map query related methods from player to map

--- Scripts/GameMap.gd
+++ Scripts/GameMap.gd
@@ -5,19 +5,47 @@ export var next_scene : String
 signal add_block
 signal game_won
 
-func _ready():
+func _enter_tree():
 	GameSingleton.set_map(self)
 
-func can_move(new_position : Vector2):
+func _ready():
+	pass	
+#	connect("player_moved", GameSingleton.get_player(), "_on_player_moved")
+
+func _on_player_moved():
+	var immediate_area : Dictionary = generate_immediate_area(GameSingleton.get_player().get_connected_blocks())
+	add_blocks_to_player(immediate_area)
+	check_win_condition(immediate_area)
+
+func to_map_scale(unit : Vector2):
+	unit.x *= cell_size.x
+	unit.y *= cell_size.y
+	return unit
+
+func round_position(position : Vector2):
+	var x : float = position.x / cell_size.x
+	var y : float = position.y / cell_size.y
+	
+	return Vector2(
+		round(position.x / cell_size.x) * cell_size.x, 
+		round(position.y / cell_size.y) * cell_size.y
+	)
+
+func can_move(movement : Vector2):
+	movement.x = movement.x * cell_size.x
+	movement.y = movement.y * cell_size.y
+	
+	var connected_blocks = GameSingleton.get_player().get_connected_blocks()
+	for block in connected_blocks:
+		if not is_open(round_position(block.global_position + movement)):
+			return false
+	return true
+
+func is_open(new_position : Vector2):
 	var new_coords = world_to_map(new_position)
 	var cell_id = get_cellv(new_coords)
 	return cell_id == -1 or tile_set.tile_get_name(cell_id) != "Wall"
 
-func _on_player_moved(blocks : Array):
-	var immediate_area : Dictionary = generate_immediate_area(blocks)
-	add_blocks_to_player(immediate_area)
-	check_win_condition(immediate_area)
-	
 func generate_immediate_area(blocks : Array):
 	var immediate_area : Dictionary = {}
 	
@@ -42,12 +70,8 @@ func add_blocks_to_player(immediate_area : Dictionary):
 			set_cellv(key, INVALID_CELL)
 
 func add_to_area(area : Dictionary, position : Vector2, is_occupied : bool):
-	
 	if area.has(position):
-		if not area[position] and is_occupied:
-			area[position] = true
-	else:
-		area[position] = is_occupied
+		area[position] = is_occupied or area[position]
 
 func check_win_condition(immediate_area : Dictionary):
 	var goal_tile_id : int = tile_set.find_tile_by_name("Goal")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,7 +3,7 @@ extends Node2D
 class_name Player
 
 onready var player 				: Sprite = $Player
-onready var connected_blocks	: Array = [self]
+onready var connected_blocks	: Array = [self] setget , get_connected_blocks
 onready var final_position		: Vector2 = global_position
 onready var initial_position	: Vector2 = global_position
 
@@ -12,8 +12,11 @@ export var is_moving		: bool = false
 
 signal player_moved
 
-func _ready():
+func _enter_tree():
 	GameSingleton.set_player(self)
+	
+func _ready():
+	pass
 
 func _process(delta):
 	var input_direction : Vector2 = Vector2(0, 0)
@@ -27,34 +30,23 @@ func _process(delta):
 	elif Input.is_action_just_pressed("ui_down"):
 		input_direction += Vector2.DOWN
 
-	if input_direction.length_squared() > 0 and can_move(input_direction):
-		if $AnimationPlayer.current_animation == "Move":
-			$AnimationPlayer.seek(0, true)
-			move_complete()
-
+	if input_direction.length_squared() > 0 and GameSingleton.get_map().can_move(input_direction):
 		initial_position = global_position
 		is_moving = true
 
+		if $AnimationPlayer.current_animation == "Move":
+			$AnimationPlayer.seek(0, true)
+			complete_move()
+		else:
+			$AnimationPlayer.play("Move")
+
 		var displacement : Vector2 = input_direction
-		displacement = to_map_scale(displacement)
+		displacement = GameSingleton.get_map().to_map_scale(displacement)
 		
 		final_position = initial_position + displacement
-		final_position = round_position(final_position)
+		final_position = GameSingleton.get_map().round_position(final_position)
 		
 		transition_value = 0
-
-		$AnimationPlayer.play("Move")
-
-func round_position(position : Vector2):
-	var cell_size : Vector2 = GameSingleton.get_map().get_cell_size()
-	var x : float = position.x / cell_size.x
-	var y : float = position.y / cell_size.y
-	
-	if $Player.centered:
-		x += 0.5
-		y += 0.5
-		
-	return Vector2(round(x) * cell_size.x, round(y) * cell_size.y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -64,24 +56,9 @@ func _physics_process(delta):
 		set_global_position(initial_position + target_offset)
 	elif global_position != final_position:
 		global_position = final_position
-
-func to_map_scale(unit : Vector2):
-	var cell_size : Vector2 = GameSingleton.get_map().get_cell_size()
-	unit.x *= cell_size.x
-	unit.y *= cell_size.y
-	return unit
-
-func can_move(movement : Vector2):
-	var map : TileMap = GameSingleton.get_map()
-	var cell_size = map.get_cell_size()
-	
-	movement.x = movement.x * cell_size.x
-	movement.y = movement.y * cell_size.y
-	
-	for block in connected_blocks:
-		if not map.can_move(round_position(block.global_position + movement)):
-			return false
-	return true
+		
+func get_connected_blocks():
+	return connected_blocks
 
 func add_block(block : Node2D):
 	block.transform.origin -= transform.origin
@@ -89,13 +66,13 @@ func add_block(block : Node2D):
 	block.set_owner(self)
 	connected_blocks.append(block)
 
-func move_complete():
-	is_moving = false
+func complete_move():
 	if global_position != final_position:
 		global_position = final_position
-	# signal move complete
+	emit_signal("player_moved")
 			
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "Move":
-		move_complete()
+		is_moving = false
+		complete_move()
 		$AnimationPlayer.play("Idle")
